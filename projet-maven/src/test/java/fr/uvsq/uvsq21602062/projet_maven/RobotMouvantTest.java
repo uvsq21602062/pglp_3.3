845f2e903a60853610b826e9d115a6c895868678
Jean Destribois <jean.destribois@gmail.com>
2020-03-27 16:54:51 +0100

    jean: Completion des classes Robot et Robot Statique et de leur classe de test

--- projet-maven/src/test/java/fr/uvsq/uvsq21602062/projet_maven/RobotMouvantTest.java
+++ projet-maven/src/test/java/fr/uvsq/uvsq21602062/projet_maven/RobotMouvantTest.java
@@ -7,8 +7,36 @@ import org.junit.Test;
 public class RobotMouvantTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetDirectionX() {
+		RobotMouvant r = new RobotMouvant(15, 16, 2, -3);
+		assertTrue(r.getDirectionX() == 2);
+	}
+	
+	@Test
+	public void testGetDirectionY() {
+		RobotMouvant r = new RobotMouvant(15, 16, 2, -3);
+		assertTrue(r.getDirectionY() == -3);
+	}
+	
+	@Test
+	public void testSetDirectionX() {
+		RobotMouvant r = new RobotMouvant(15, 16, 2, -3);
+		r.setDirectionX(3);
+		assertTrue(r.getDirectionX() == 3);
+	}
+	
+	@Test
+	public void testSetDirectionY() {
+		RobotMouvant r = new RobotMouvant(15, 16, 2, -3);
+		r.setDirectionY(3);
+		assertTrue(r.getDirectionY() == 3);
+	}
+	
+	@Test
+	public void testAvancer() {
+		RobotMouvant r = new RobotMouvant(15, 16, 2, -3);
+		r.avancer();
+		assertTrue(r.getPositionX() == 17 && r.getPositionY() == 13);
 	}
 
 }
--- projet-maven/src/test/java/fr/uvsq/uvsq21602062/projet_maven/RobotTest.java
+++ projet-maven/src/test/java/fr/uvsq/uvsq21602062/projet_maven/RobotTest.java
@@ -7,8 +7,29 @@ import org.junit.Test;
 public class RobotTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetPositionX() {
+		Robot r = new Robot(15, 16);
+		assertTrue(r.getPositionX() == 15);
+	}
+	
+	@Test
+	public void testGetPositionY() {
+		Robot r = new Robot(15, 16);
+		assertTrue(r.getPositionY() == 16);
+	}
+	
+	@Test
+	public void testSetPositionX() {
+		Robot r = new Robot(15, 16);
+		r.setPositionX(10);
+		assertTrue(r.getPositionX() == 10);
+	}
+	
+	@Test
+	public void testSetPositionY() {
+		Robot r = new Robot(15, 16);
+		r.setPositionY(10);
+		assertTrue(r.getPositionY() == 10);
 	}
 
 }
