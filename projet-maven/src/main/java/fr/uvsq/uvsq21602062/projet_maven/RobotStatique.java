97586c5022c3d40f246898b052750793b644a60a
Jean Destribois <jean.destribois@gmail.com>
2020-03-27 17:18:40 +0100

    jean: dernier commit, fin de l'exercice

--- projet-maven/src/main/java/fr/uvsq/uvsq21602062/projet_maven/RobotStatique.java
+++ projet-maven/src/main/java/fr/uvsq/uvsq21602062/projet_maven/RobotStatique.java
@@ -1,5 +1,7 @@
 package fr.uvsq.uvsq21602062.projet_maven;
 
-public class RobotStatique {
-
+public class RobotStatique extends Robot {
+	RobotStatique(int positionX, int positionY) {
+		super(positionX, positionY);
+	}
 }
--- projet-maven/src/test/java/fr/uvsq/uvsq21602062/projet_maven/RobotStatiqueTest.java
+++ projet-maven/src/test/java/fr/uvsq/uvsq21602062/projet_maven/RobotStatiqueTest.java
@@ -6,9 +6,5 @@ import org.junit.Test;
 
 public class RobotStatiqueTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
 
 }
